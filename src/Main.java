feab3b19ff5dbe48acd33c2604d93c0cf4c3890a
tjmichiels <thomasmichiels0@gmail.com>
2024-05-24 12:12:54 +0200

    onnodige API calls weggehaald

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Object[]> postcodesEnHuisnummers = new ArrayList<>();
+        postcodesEnHuisnummers.add(new Object[]{"3605KW", 1180});
+        postcodesEnHuisnummers.add(new Object[]{"3995DP", 130});
+        postcodesEnHuisnummers.add(new Object[]{"3543AJ", 80});
+        postcodesEnHuisnummers.add(new Object[]{"1211DZ", 1});
+        postcodesEnHuisnummers.add(new Object[]{"1211CB", 10});
+        postcodesEnHuisnummers.add(new Object[]{"1217ET", 62});
+        postcodesEnHuisnummers.add(new Object[]{"1315HD", 5});
+        postcodesEnHuisnummers.add(new Object[]{"1315BP", 36});
+        postcodesEnHuisnummers.add(new Object[]{"3621ZA", 1});
         // Deze graag als commentaar laten want we hebben beperkte API calls
-//        ArrayList<Object[]> postcodesEnHuisnummers = new ArrayList<>();
-//        postcodesEnHuisnummers.add(new Object[]{"3605KW", 1180});
-//        postcodesEnHuisnummers.add(new Object[]{"3995DP", 130});
-//        postcodesEnHuisnummers.add(new Object[]{"3543AJ", 80});
-//        postcodesEnHuisnummers.add(new Object[]{"1211DZ", 1});
-//        postcodesEnHuisnummers.add(new Object[]{"1211CB", 10});
-//        postcodesEnHuisnummers.add(new Object[]{"1217ET", 62});
-//        postcodesEnHuisnummers.add(new Object[]{"1315HD", 5});
-//        postcodesEnHuisnummers.add(new Object[]{"1315BP", 36});
-//        postcodesEnHuisnummers.add(new Object[]{"3621ZA", 1});
 //        CreateFile.createTSPFile("route", postcodesEnHuisnummers);
 
 
